a9fe58a2079b8935a563f7208da630d89e259ad7
Ivan Maltese <ivan.maltese@objectmethod.it>
2021-02-22 11:59:31 +0100

    allineamento

--- src/main/java/it/objectmethod/tutorial/tutorial/service/mapper/CarMapper.java
+++ src/main/java/it/objectmethod/tutorial/tutorial/service/mapper/CarMapper.java
@@ -1,7 +1,5 @@
 package it.objectmethod.tutorial.tutorial.service.mapper;
 
-import java.util.List;
-
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -11,12 +9,13 @@ import it.objectmethod.tutorial.tutorial.service.dto.CarDTO;
 @Mapper(componentModel = "spring")
 public interface CarMapper extends EntityMapper<CarDTO, CarEntity> {
 
-	@Mapping(source="brand.id", target="brandId")
-	@Mapping(source="brand.brandName", target="brandName")
+	@Override
+	@Mapping(source = "brand.id", target = "brandId")
+	@Mapping(source = "brand.brandName", target = "brandName")
 	CarDTO toDto(CarEntity entity);
 
+	@Override
 	@Mapping(target = "brand", ignore = true)
 	CarEntity toEntity(CarDTO dto);
-	
-	List<CarDTO> toDto(List<CarEntity> entityList);
+
 }
